cbb25dec39b1edac2cddb05f2cb31bb60f360f3e
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-04-04 16:45:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,41 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T* data = {};
+  int top = 1, size = 0;
+  void resize(int nsize) {
+    T* temp = new T[nsize];
+    for (int i = 0; i < size; i++) {
+      temp[i] = data[i];
+    }
+    delete data;
+    data = temp;
+    size = nsize;
+  }
+
+ public:
+  TStack() {
+    data = new T[SIZE];
+    size = SIZE;
+    top = 1;
+  }
+  ~TStack() { delete[] data; }
+  void push(T item) {
+    if (top >= size) {
+      resize(size * 2);
+    }
+    data[top] = item;
+    top++;
+  }
+  T pop() {
+    if (top == 1) {
+      throw "Stack is empty!";
+    } else {
+      top -= 1;
+      return data[top];
+    }
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
